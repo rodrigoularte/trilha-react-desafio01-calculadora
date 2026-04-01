1388584d136207ad3e9fc830a938174dd6179ae6
Rodrigo Goularte <rodrigo.r.goularte@hotmail.com>
2026-04-01 18:22:40 -0300

    feat: create handleCalculation function and call it on operation buttons

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,6 @@ const App = () => {
   const [operation, setOperation] = useState('');
   const [calculation, setCalculation] = useState('');
 
-  console.log({
-    currentNumber,
-    firstNumber,
-    operation,
-    calculation,
-  });
-
-  const handleOnClear = () => {
-    setCurrentNumber('0');
-    setFirstNumber('0');
-    setOperation('');
-    setCalculation('');
-  };
-
   const handleAddNumber = (num: string) => {
     setCurrentNumber((prev) => {
       if (num === '.' && prev.includes('.')) {
@@ -38,48 +24,65 @@ const App = () => {
     });
   };
 
-  const handleCalculate = (action: string) => {
-    if (firstNumber === '0') {
-      setFirstNumber(String(currentNumber));
-      setCalculation(`${currentNumber.replace('.', ',')} ${action}`);
-      setCurrentNumber('0');
+  const handleCalculation = (action: string) => {
+    if (operation === '') {
       setOperation(action);
-    } else {
-      switch (action) {
-        case '+':
-          const addition = Number(firstNumber) + Number(currentNumber);
-          setCalculation(`${calculation} ${currentNumber} =`);
-          setCurrentNumber(String(addition));
-          setOperation('');
-          break;
-        case '-':
-          const subtraction = Number(firstNumber) - Number(currentNumber);
-          setCalculation(`${calculation} ${currentNumber} =`);
-          setCurrentNumber(String(subtraction));
-          setOperation('');
-          break;
-        case 'x':
-          const multiplication = Number(firstNumber) * Number(currentNumber);
-          setCalculation(`${calculation} ${currentNumber} =`);
-          setCurrentNumber(String(multiplication));
-          setOperation('');
-          break;
-        case '÷':
-          const division = Number(firstNumber) / Number(currentNumber);
-          setCalculation(`${calculation} ${currentNumber} =`);
-          setCurrentNumber(String(division));
-          setOperation('');
-          break;
-        default:
-          break;
-      }
+      setCalculation(`${currentNumber.replace('.', ',')} ${action}`);
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');
+      return;
+    }
+
+    if (action !== operation) {
+      setOperation(action);
+
+      setCalculation(`${firstNumber.replace('.', ',')} ${action}`);
+      return;
+    }
+  };
+
+  const getResult = (action: string) => {
+    switch (action) {
+      case '+':
+        const addition = Number(firstNumber) + Number(currentNumber);
+        setCalculation(`${calculation} ${currentNumber} =`);
+        setCurrentNumber(String(addition));
+        setOperation('');
+        break;
+      case '-':
+        const subtraction = Number(firstNumber) - Number(currentNumber);
+        setCalculation(`${calculation} ${currentNumber} =`);
+        setCurrentNumber(String(subtraction));
+        setOperation('');
+        break;
+      case 'x':
+        const multiplication = Number(firstNumber) * Number(currentNumber);
+        setCalculation(`${calculation} ${currentNumber} =`);
+        setCurrentNumber(String(multiplication));
+        setOperation('');
+        break;
+      case '÷':
+        const division = Number(firstNumber) / Number(currentNumber);
+        setCalculation(`${calculation} ${currentNumber} =`);
+        setCurrentNumber(String(division));
+        setOperation('');
+        break;
+      default:
+        break;
     }
   };
 
   const handleEquals = () => {
     if (firstNumber !== '0' && operation !== '' && currentNumber !== '0') {
-      handleCalculate(operation);
+      getResult(operation);
     }
+  };
+
+  const handleOnClear = () => {
+    setCurrentNumber('0');
+    setFirstNumber('0');
+    setOperation('');
+    setCalculation('');
   };
 
   return (
@@ -99,25 +102,25 @@ const App = () => {
           <Button label="7" onClick={() => handleAddNumber('7')} />
           <Button label="8" onClick={() => handleAddNumber('8')} />
           <Button label="9" onClick={() => handleAddNumber('9')} />
-          <Button label="÷" onClick={() => handleCalculate('÷')} />
+          <Button label="÷" onClick={() => handleCalculation('÷')} />
         </Row>
         <Row>
           <Button label="4" onClick={() => handleAddNumber('4')} />
           <Button label="5" onClick={() => handleAddNumber('5')} />
           <Button label="6" onClick={() => handleAddNumber('6')} />
-          <Button label="x" onClick={() => handleCalculate('x')} />
+          <Button label="x" onClick={() => handleCalculation('x')} />
         </Row>
         <Row>
           <Button label="1" onClick={() => handleAddNumber('1')} />
           <Button label="2" onClick={() => handleAddNumber('2')} />
           <Button label="3" onClick={() => handleAddNumber('3')} />
-          <Button label="-" onClick={() => handleCalculate('-')} />
+          <Button label="-" onClick={() => handleCalculation('-')} />
         </Row>
         <Row>
           <Button label="0" onClick={() => handleAddNumber('0')} />
           <Button label="," onClick={() => handleAddNumber('.')} />
           <Button label="=" onClick={handleEquals} />
-          <Button label="+" onClick={() => handleCalculate('+')} />
+          <Button label="+" onClick={() => handleCalculation('+')} />
         </Row>
       </Content>
     </Container>
